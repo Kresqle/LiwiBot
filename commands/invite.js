ec83b087f4dd41c1243ec93d4a6dd518505f8633
Kresqle <75183858+Kresqle@users.noreply.github.com>
2021-03-18 21:54:49 +0100

    Add files via upload

--- commands/invite.js
+++ commands/invite.js
@@ -1,7 +1,7 @@
 module.exports = {
-  name: "invite",
-  description: "Get the invite link of the bot",
-  execute(message) {
+  name: 'invite',
+  description: 'Get the invite link of the bot',
+  execute (message) {
     message.channel.send('Here is my invite link, <@' + message.author.id + '>\nhttps://discord.com/api/oauth2/authorize?client_id=816486025592307722&permissions=8&scope=bot')
   }
 }
